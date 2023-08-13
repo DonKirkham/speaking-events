f3a481f79a7d5a7217dcbd991adc475fd2308572
Don Kirkham <donkirkham@live.com>
2023-08-13 10:03:53 -0500

    Initial Commit

---
+++ src/webparts/speakingEvents/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISpeakingEventsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SpeakingEventsWebPartStrings' {
+  const strings: ISpeakingEventsWebPartStrings;
+  export = strings;
+}
