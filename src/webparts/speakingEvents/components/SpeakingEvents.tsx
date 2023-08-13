7c59738ecb0da31d3dfcd80cece58952e80ab50c
Don Kirkham <donkirkham@live.com>
2023-08-13 12:43:38 -0500

    Added Hooks

--- src/webparts/speakingEvents/components/SpeakingEvents.tsx
+++ src/webparts/speakingEvents/components/SpeakingEvents.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import styles from './SpeakingEvents.module.scss';
 import { escape } from '@microsoft/sp-lodash-subset';
 
@@ -13,24 +14,45 @@ export interface ISpeakingEventsProps {
   userDisplayName: string;
 }
 
-export default class SpeakingEvents extends React.Component<ISpeakingEventsProps, {}> {
-  public render(): React.ReactElement<ISpeakingEventsProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
+export const SpeakingEvents: React.FC<ISpeakingEventsProps> = (props) => {
+  const {
+    description,
+    isDarkTheme,
+    environmentMessage,
+    hasTeamsContext,
+    userDisplayName
+  } = props;
 
-    return (
-      <section className={`${styles.speakingEvents} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <ul className={styles.links}>
-          </ul>
-        </div>
-      </section>
-    );
+  const [counter, setCounter] = useState<number>(0);
+  const [oddEven, setOddEven] = useState<string>('');
+
+  useEffect(() => {
+    console.log("useEffect([]) called");
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect([counter]) called");
+    setOddEven(counter % 2 === 0 ? 'even' : 'odd');
+  }, [counter]);
+
+  const onCounterButtonClicked = () => {
+    console.log("onCounterButtonClicked() called");
+    setCounter(counter + 1);
   }
+  
+  console.log("Render() called");
+  return (
+    <section className={`${styles.speakingEvents} ${hasTeamsContext ? styles.teams : ''}`}>
+      <div className={styles.welcome}>
+        <h3>Welcome to SharePoint Framework!</h3>
+        <p>Counter: <strong>{counter}</strong></p>
+        <p>Counter is <strong>{oddEven}</strong></p>
+        <p><button onClick={() => onCounterButtonClicked()}>Click Me!!</button></p>
+        <ul className={styles.links}>
+        </ul>
+      </div>
+    </section>
+  );
 }
+
+export default SpeakingEvents;
